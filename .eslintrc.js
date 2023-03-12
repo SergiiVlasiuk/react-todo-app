05ac8aed570e3c7c8f7ce4a6e6002f3683b93bf2
Serhii Vlasiuk <sergii_vlasiuk@ukr.net>
2023-03-12 23:31:00 +0200

    fix js configuration to use auto import js files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es2021  : true,
     node    : true,
   },
+  // extends       : ['eslint:recommended', 'plugin:react/recommended', 'plugin:json/recommended', 'prettier'],
   extends       : ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   overrides     : [],
   parserOptions : {
